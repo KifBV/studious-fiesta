fe170b983c53e4ddde2dee06f54ce906332e8e37
Franck Ratier <ratier.franck@gmail.com>
2017-06-20 17:46:55 +0300

    working cluster

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -21,35 +21,35 @@ resource "digitalocean_droplet" "rancher-server" {
   user_data = "${var.rancher_server_user_data}"
 }
 
-#resource "digitalocean_droplet" "rancher-host" {
-#  count     = "${var.droplet_count}"
-#  name      = "${var.name}-host-${count.index}"
-#  size      = "${var.droplet_size}"
-#  image     = "${var.droplet_image}"
-#  region    = "${var.region}"
-#  ssh_keys  = ["${var.droplet_ssh_keys}"]
-#  user_data = "${var.rancher_host_user_data}"
-#}
-#
-## LOAD BALANCER
-#resource "digitalocean_loadbalancer" "mod" {
-#  name   = "${var.name}-${var.lb_prefix}"
-#  region = "${var.region}"
-#
-#  forwarding_rule {
-#    entry_port      = 80
-#    entry_protocol  = "http"
-#    target_port     = 80
-#    target_protocol = "http"
-#  }
-#
-#  healthcheck {
-#    port     = 22
-#    protocol = "tcp"
-#
-#    healthy_threshold   = 2
-#    unhealthy_threshold = 2
-#  }
-#
-#  droplet_ids = ["${digitalocean_droplet.rancher-host.*.id}"]
-#}
+resource "digitalocean_droplet" "rancher-host" {
+  count     = "${var.droplet_count}"
+  name      = "${var.name}-host-${count.index}"
+  size      = "${var.droplet_size}"
+  image     = "${var.droplet_image}"
+  region    = "${var.region}"
+  ssh_keys  = ["${var.droplet_ssh_keys}"]
+  user_data = "${var.rancher_host_user_data}"
+}
+
+# LOAD BALANCER
+resource "digitalocean_loadbalancer" "mod" {
+  name   = "${var.name}-${var.lb_prefix}"
+  region = "${var.region}"
+
+  forwarding_rule {
+    entry_port      = 80
+    entry_protocol  = "http"
+    target_port     = 80
+    target_protocol = "http"
+  }
+
+  healthcheck {
+    port     = 22
+    protocol = "tcp"
+
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  droplet_ids = ["${digitalocean_droplet.rancher-host.*.id}"]
+}
